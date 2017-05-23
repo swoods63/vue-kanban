011fe4838e1ec3f1769074013b11c5fec0d360a5
Susan Woods <swoods63@msn.com>
2017-05-23 13:47:58 -0600

    made

--- server/config/env.js
+++ server/config/env.js
@@ -3,10 +3,10 @@ let env = {
 	NODE_ENV: 'development',
 	PORT: 3000,
 	DBPROTOCOL: 'mongodb',
-	DBUSERNAME: 'student',
-	DBPASSWORD: 'CodeWorksStudent',
-	DBHOST: 'ds056789.mlab.com:56789',
-	DBNAME: 'codeworks',
+	DBUSERNAME: 'dustysue',
+	DBPASSWORD: 'dustysue',
+	DBHOST: 'ds149491.mlab.com:49491',
+	DBNAME: 'kanbandustysue',
 	SERVERNAME: 'dev-server'
 }
 
